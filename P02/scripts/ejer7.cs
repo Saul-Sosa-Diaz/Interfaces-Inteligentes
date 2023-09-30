a94a68e69c4581b45a683d0d7ba294d7b4751152
Saul Sosa <alu0101404141@ull.edu.es>
2023-09-30 20:49:40 +0100

    Arreglado el ejercicio7

--- P02/scripts/ejer7.cs
+++ P02/scripts/ejer7.cs
@@ -12,10 +12,17 @@ public class ejer7 : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.A)) {
             Material m_Material;
             m_Material = GetComponent<Renderer>().material;
+            Color newColor = new Color(UnityEngine.Random.Range(0.0f,0.9f),UnityEngine.Random.Range(0.0f,0.9f), UnityEngine.Random.Range(0.0f,0.9f));
+            m_Material.color = newColor;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            Material m_Material;
+            m_Material = GameObject.FindWithTag("cube").GetComponent<Renderer>().material;
             Color newColor = new Color(UnityEngine.Random.Range(0.0f,0.9f),UnityEngine.Random.Range(0.0f,0.9f), UnityEngine.Random.Range(0.0f,0.9f));
             m_Material.color = newColor;
         }
